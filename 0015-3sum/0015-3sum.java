5bdc579fd065c3c5cafa91d0bb3e222a3494348b
Shakshi Jha <shakshijha06@gmail.com>
2026-07-27 16:34:12 +0530

    Time: 31 ms (83.15%), Space: 59.2 MB (60.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,53 +1,40 @@
 import java.util.*;
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        // Sort the array
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Early break: if the smallest element is positive, no triplet can sum to 0
-            if (nums[i] > 0) {
-                break;
-            }
-            
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                
-                if (total == 0) {
-                    // Found a valid triplet
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
+class Solution{
+    public List<List<Integer>>threeSum(int[] num) {
+        Arrays.sort(num);
+        int n=num.length;
+        List<List<Integer>> result= new ArrayList<>();
+        for (int i= 0;i<n-2;i++)
+        {
+            if(num[i]>0)
+            break;
+            if (i>0 && num[i]==num[i-1])
+            continue;
+            if(num[i]+num[i+1]+num[i+2]>0)
+            break;
+            if(num[i]+num[n-2]+num[n-1]<0)
+            continue;
+            int left=i+1;
+            int right=n-1;
+            int first=num[i];
+            while(left<right)
+            {
+                int total= first+num[left]+num[right];
+                if(total==0)
+                {
+                    result.add(Arrays.asList(first,num[left],num[right]));
                     left++;
                     right--;
-                    
-                    // Skip duplicate left values
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-                    
-                    // Skip duplicate right values
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                    
-                } else if (total < 0) {
-                    left++; // Need a larger sum
-                } else {
-                    right--; // Need a smaller sum
+                    while(left<right && num[left]==num[left-1]){
+                    left++;}
+                    while(left<right && num[right]==num[right+1]){
+                    right--;}
+
                 }
+                else if(total<0){
+                left++;}
+                else { right--;}
             }
-        }
-        
-        return ans;
+        } return result;
     }
 }
