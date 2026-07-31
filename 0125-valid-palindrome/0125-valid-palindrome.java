803ab8942331e432e528df1c489321d21e1a49e1
Shakshi Jha <shakshijha06@gmail.com>
2026-07-31 22:04:35 +0530

    Time: 2 ms (99.32%), Space: 44 MB (92.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,19 +2,20 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left=0;
         int right=s.length()-1;
-        while(left<right)
+        while(left<right)//move till left is less than right
         {
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left)))
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left)))//check if any non alphanumeric character in any index left encounter
             { 
-                left++;
+                left++;//increment left if yes
             }
-            while(left<right && !Character.isLetterOrDigit(s.charAt(right)))
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right)))//check if any non alphanumeric characters in any index right encounters 
             {
-                right--;
+                right--;//decrement right if yes
             }
-            if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right)))
+            if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right)))//compare characters 
             {
-                return false;}
+                return false;
+                }
                 left++;
                 right--;
             
